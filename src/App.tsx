c0ef146ef55412b4907b96ccab03489ef02bbef0
Liliia Nikitchenko <liliia.nikitchenko@hec.de>
2026-01-08 09:30:06 +0100

    authentication for the entire website

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,34 @@ import RequireAuth from "./auth/RequireAuth.tsx";
 import {EmployeeTable} from "./pages/EmployeeTable.tsx";
 
 function App() {
-
-
     return (
-        <Container>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/foo">Foo</Nav.Link>
-                            <Nav.Link as={Link} to="/bar">Bar</Nav.Link>
-                            <Nav.Link as={Link} to="/employees">Mitarbeiter</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/foo" element={<UnsecuredFoo/>}/>
-                <Route path="/bar" element={
-                    <RequireAuth>
+        <RequireAuth>
+            <Container>
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link as={Link} to="/foo">Foo</Nav.Link>
+                                <Nav.Link as={Link} to="/bar">Bar</Nav.Link>
+                                <Nav.Link as={Link} to="/employees">Mitarbeiter</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/foo" element={<UnsecuredFoo/>}/>
+                    <Route path="/bar" element={
                         <SecuredBar/>
-                    </RequireAuth>
-                }/>
-                <Route path="/employees" element={
-                    <RequireAuth>
+                    }/>
+                    <Route path="/employees" element={
                         <EmployeeTable/>
-                    </RequireAuth>
-                }/>
-            </Routes>
-        </Container>
+                    }/>
+                </Routes>
+            </Container>
+        </RequireAuth>
     )
 }
 
