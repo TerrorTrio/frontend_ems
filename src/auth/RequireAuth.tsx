7a9b794225bf81fd0a1220398fcfcd72e6998b33
Marvin Meiling <marvin.meiling@hec.de>
2026-01-14 13:58:34 +0100

    fix: Coloration

--- src/auth/RequireAuth.tsx
+++ src/auth/RequireAuth.tsx
@@ -6,7 +6,6 @@ export default function RequireAuth({children}: { children: JSX.Element }) {
 
     if (auth.isLoading) return <p>Lädt…</p>;
     if (auth.error) return <p>Fehler: {auth.error.message}</p>;
-
     if (!auth.isAuthenticated) {
         auth.signinRedirect();
         return <div>Weiterleitung zum Login...</div>;
--- src/components/menue.tsx
+++ src/components/menue.tsx
@@ -4,15 +4,15 @@ import "../App.css";
 export default function Menue() {
     return (
         <aside className="sidebar">
-            <nav className="menue">
-                <ul className="menue-list">
+            <nav className="nav">
+                <ul className="nav-list">
                     <li className="menue-item">
-                        <NavLink to="/employees" className="menue-link">
+                        <NavLink to="/employees" className="nav-link">
                             Mitarbeiter
                         </NavLink>
                     </li>
                     <li className="menue-item">
-                        <NavLink to="/qualifications" className="menue-link">
+                        <NavLink to="/qualifications" className="nav-link">
                             Qualifikationen
                         </NavLink>
                     </li>
