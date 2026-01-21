4db77cab30a39d487e9aad3a01cb8e7b0a6e263b
Marvin Meiling <marvin.meiling@hec.de>
2026-01-21 11:52:53 +0100

    added route to EmployeePage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
 import NavBar from "./components/Navbar.tsx";
+import {Route, Routes} from "react-router-dom";
+import {EmployeePage} from "./pages/EmployeePage.tsx";
 
 function App() {
     return (
@@ -9,8 +11,10 @@ function App() {
             <Header/>
             <div style={{display: "flex", flex: 1, minHeight: 0}}>
                 <NavBar/>
+                <Routes>
+                    <Route path="/employees" element={<EmployeePage/>}/>
+                </Routes>
             </div>
-
             <Footer/>
         </div>
     );
